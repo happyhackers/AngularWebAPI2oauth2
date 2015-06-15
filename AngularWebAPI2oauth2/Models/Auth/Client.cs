7f0a04e7642dc67cde5dfd230375e88115da84d5
Kalle Niklasson <kalle@aotp.se>
2015-06-16 00:28:45 +0200

    More comments

--- AngularWebAPI2oauth2/Models/Auth/Client.cs
+++ AngularWebAPI2oauth2/Models/Auth/Client.cs
@@ -2,18 +2,27 @@
 
 namespace AngularWebAPI2oauth2.Models.Auth
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class Client
     {
+        /// <remarks/>
         [Key]
         public string Id { get; set; }
+        /// <remarks/>
         [Required]
         public string Secret { get; set; }
-        [Required]
-        [MaxLength(100)]
+        /// <remarks/>
+        [Required, MaxLength(100)]
         public string Name { get; set; }
+        /// <remarks/>
         public ApplicationTypes ApplicationType { get; set; }
+        /// <remarks/>
         public bool Active { get; set; }
+        /// <remarks/>
         public int RefreshTokenLifeTime { get; set; }
+        /// <remarks/>
         [MaxLength(100)]
         public string AllowedOrigin { get; set; }
     }
