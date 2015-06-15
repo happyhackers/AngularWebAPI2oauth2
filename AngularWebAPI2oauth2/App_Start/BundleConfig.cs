c7305a0453170b3574402b039293a5ca011ad37f
moogiio <tvingmark@gmail.com>
2015-06-15 22:26:27 +0200

    Rearranged Content

--- AngularWebAPI2oauth2/App_Start/BundleConfig.cs
+++ AngularWebAPI2oauth2/App_Start/BundleConfig.cs
@@ -8,47 +8,56 @@ namespace AngularWebAPI2oauth2
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.min.css",
-                      "~/Content/Site.css",
-                      "~/Content/font-awesome.min.css"));
+                        "~/Content/Bootstrap/bootstrap.min.css",
+                        "~/Content/Font-Awesome/font-awesome.min.css",
+                        "~/Content/Site.css",
+                        "~/Content/CSS-Reset/CSSReset.css"
+                        ));
 
             bundles.Add(new ScriptBundle("~/bundles/vendor/external").Include(
                         "~/Scripts/vendor/jquery-{version}.js",
                         "~/Scripts/vendor/bootstrap.js",
-                        "~/Scripts/vendor/respond.js"));
+                        "~/Scripts/vendor/respond.js"
+                        ));
 
             bundles.Add(new ScriptBundle("~/bundles/vendor/angular").Include(
-                      "~/Scripts/vendor/angular.min.js",
-                      "~/Scripts/vendor/angular-ui-router.min.js",
-                      "~/Scripts/vendor/angular-animate.min.js",
-                      "~/Scripts/vendor/angular-resource.min.js",
-                      "~/Scripts/vendor/loading-bar.js",
-                      "~/Scripts/vendor/angular-local-storage.min.js"
-                      ));
+                        "~/Scripts/vendor/angular.min.js",
+                        "~/Scripts/vendor/angular-ui-router.min.js",
+                        "~/Scripts/vendor/angular-animate.min.js",
+                        "~/Scripts/vendor/angular-resource.min.js",
+                        "~/Scripts/vendor/loading-bar.js",
+                        "~/Scripts/vendor/angular-local-storage.min.js"
+                        ));
 
             bundles.Add(new ScriptBundle("~/bundles/app/modules").IncludeDirectory(
-                      "~/Scripts/app/modules/",
-                      "*.js",
-                      true));
+                        "~/Scripts/app/modules/",
+                        "*.js",
+                        true
+                        ));
 
             bundles.Add(new ScriptBundle("~/bundles/app/services").IncludeDirectory(
-                      "~/Scripts/app/services/",
-                      "*.js",
-                      true));
+                        "~/Scripts/app/services/",
+                        "*.js",
+                        true
+                        ));
+
             bundles.Add(new ScriptBundle("~/bundles/app/controllers").IncludeDirectory(
-                      "~/Scripts/app/controllers/",
-                      "*.js",
-                      true));
+                        "~/Scripts/app/controllers/",
+                        "*.js",
+                        true
+                        ));
 
             bundles.Add(new ScriptBundle("~/bundles/app/filters").IncludeDirectory(
-                      "~/Scripts/app/filters/",
-                      "*.js",
-                      true));
+                        "~/Scripts/app/filters/",
+                        "*.js",
+                        true
+                        ));
 
             bundles.Add(new ScriptBundle("~/bundles/app/directives").IncludeDirectory(
-                      "~/Scripts/app/directives/",
-                      "*.js",
-                      true));
+                        "~/Scripts/app/directives/",
+                        "*.js",
+                        true
+                        ));
 
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
